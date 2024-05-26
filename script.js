a453b6097ac2b517775a5c46b23286b79a38bf20
Jishan <jahedulislamdev@gmail.com>
2024-05-26 23:14:45 +0600

    gallery modal style and others

--- script.js
+++ script.js
@@ -24,41 +24,42 @@ var swiper = new Swiper(".mySwiper", {
     },
   }
 });
-// swiper carousel end here
 // scrolling animaition start here
 const animatedSections = document.querySelectorAll(".inanimation");
-// console.log(animatedSections);
 window.onscroll = () => { 
   animatedSections.forEach( section => { 
     let top = window.scrollY;
     let offset = section.offsetTop -190;
     let height = section.offsetHeight;
-
     if(top >= offset && top < offset + height){ 
       section.classList.add('show_animate');
     }
-    else{
-      section.classList.remove('show_animate')
-    }
+    // else{
+    //   section.classList.remove('show_animate')
+    // }
   } )
 }
-// scrolling animaition end here
 
 // gallery modal script 
 document.addEventListener('click' , (e)=> { 
-  // console.log(e.target)
   if(e.target.classList.contains("gallery_img")){
     const src = e.target.getAttribute("src");
-    // console.log(src);
     document.querySelector(".modal_img").src = src;
     const myModal = new bootstrap.Modal(document.getElementById('galleryModal'))
     myModal.show();
   }
 })
-// see more 
-const see = document.querySelector("#see");
-const more = document.querySelector("#more");
-see.addEventListener("click", () => { 
-  more.style.display = "block";
-  see.style.display = "none";
-})
+// modal image zoom in and out function
+const ZoomInOut = ()=> { 
+  const galleryImgId = document.getElementById('galleryImgId');
+  const zoomIn = document.querySelector('.zooIn_button');
+  const zoomOut = document.querySelector('.zooOut_button');
+
+  zoomIn.addEventListener('click',()=> { 
+    galleryImgId.classList.add('zoomed')
+  })
+  zoomOut.addEventListener('click', ()=> { 
+    galleryImgId.classList.remove('zoomed')
+  })
+}
+ZoomInOut();
